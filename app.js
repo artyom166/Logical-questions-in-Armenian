159b261203be315f41a22f2c4e65e57d429d4610
artyom <126690727+artyom166@users.noreply.github.com>
2023-03-02 20:07:28 +0400

    Logical questions in Armenian
    Logical questions in Armenian․ If there is a problem with the program, please let me know

---
+++ app.js
@@ -0,0 +1,387 @@
+/* Created and coded by Abhilash Narayan */
+/* Quiz source: w3schools.com */
+var quiz = {
+    "JS": [
+    {
+    "id": 1,
+    "question": "ով է ստեղծել հայոց տառերը",
+    "options": [
+    {
+    "a": "Մեսրոպ Մաշտոցը",
+    "b": "Հովհաննես Թումանյան",
+    "c": "Լեոնարդո Դա Վինչի",
+    "d": "Սահակ Պարթև"
+    }
+    ],
+    "answer": "Մեսրոպ Մաշտոցը",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 2,
+    "question": "Ձին տեղափոխում է 10կգ ածուխ, իսկ պոնին՝ 10կգ բամբակ։ Ո՞ւմ բեռն է ավելի ծանր",
+    "options": [
+    {
+    "a": "Պոնի",
+    "b": "հավասար են",
+    "c": "Ձի"
+    }
+    ],
+    "answer": "հավասար են",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 3,
+    "question": "Ի՞նչ կա երկրի կենտրոնում։",
+    "options": [
+    {
+    "a": "այլ պատասխան",
+    "b": "Քաղաք",
+    "c": "կ տառը"
+    }
+    ],
+    "answer": "կ տառը",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 4,
+    "question": "Տղամարդը գնում է գնացքով և երազում տեսնում, որ նա նստած է հատակի վրա, իսկ հատակի տախտակները պոկվում ու ընկնում են ցած։ Ու միայն մի տախտակ է մնացել։ Ի՞նչ պետք է անի տղամարդը, որպեսզի չընկնի։",
+    "options": [
+    {
+    "a": "պետք է արթնանա",
+    "b": "պետք է տախտակից պինդ բռնվի"
+    }
+    ],
+    "answer": "պետք է արթնանա",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 5,
+    "question": "Որտե՞ղ է հնարավոր, որ ձին թռչի ձիու վրայով։",
+    "options": [
+    {
+    "a": "շախմատում",
+    "b": "Խաղերում",
+    "c": "Երազում",
+    "d": "Չգիտեմ",
+    }
+    ],
+    "answer": "շախմատում",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 6,
+    "question": "Մրգամանի մեջ մրգեր կան։ Նարինջներն ավելի շատ են, քան բանանները և ավելի քիչ, քան սալորները։ Ո՞ր մրգերն են մրգամանում ամենաքիչը։",
+    "options": [
+    {
+    "a": "սալորները ու բանաները",
+    "b": " բանանները",
+    "c": "նարինջները",
+    }
+    ],
+    "answer": " բանանները",
+    "score": 0,
+    "status": ""
+    },
+
+    {
+    "id": 7,
+    "question": "Տոնի ժամանակ երեխաներին շոկոլադե ռուլետ նվիրեցին։ Սեղանի մոտ նստած են 10 երեխա։ Քանի՞ անգամ դանակով պետք է կտրվածք անել, որպեսզի յուրաքանչյուր երեխա ստանա մեկական կտոր տորթ,",
+    "options": [
+    {
+    "a": "10",
+    "b": "11",
+    "c": "9",
+    }
+    ],
+    "answer": "9",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 8,
+    "question": "Քանի աչքեր տեսան շաղգամը «Պապն ու շաղգամը» առակի մեջ։",
+    "options": [
+    {
+    "a": "12 աչքեր",
+    "b": ". 2 աչքեր ",
+    "c": " 6 աչքեր",
+    "d": "8 աչքեր",
+    },
+    ],
+    "answer": "12 աչքեր",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 9,
+    "question": "Կորյունի պայուսակում կա 2կգ երկաթ, Աստղիկի պայուսակում՝ 2կգ կոնֆետ, իսկ Տրդատի պայուսակում՝ 2կգ բամբակ։ Ում պայուսակն է ծանր։",
+    "options": [
+    {
+    "a": "Կորյունի",
+    "b": "Տրդատի",
+    "c": "Աստղիկի",
+    "d": "3 պայուսակների քաշը նույնն է",
+    },
+    ],
+    "answer": "3 պայուսակների քաշը նույնն է",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 10,
+    "question": "Լծասայլին կապված 6 ձի ճանապարհն անցան 4 ժամում։ Նրանցից յուրաքանչյուրը որքա՞ն ժամ վազեց։",
+    "options": [
+    {
+    "a": "10 ժամ",
+    "b": "4 ժամ",
+    "c": "24 ժամ",
+    "d": "2 ժամ",
+    },
+    ],
+    "answer": "4 ժամ",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 11,
+    "question": "Ո՞ր թիվն է համապատասխանում իր անունի տառերի քանակին։",
+    "options": [
+    {
+    "a": "6",
+    "b": "4",
+    "c": "1",
+    "d": "10"
+    },
+    ],
+    "answer": "4",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 12,
+    "question": "Առավոտյան ջերմաչափը ցույց է տալիս 10 աստիճան սառնամանիք։ Ճաշին դրսում օդը տաքացավ 6 աստիճանով։ Ջերմաչափի ցուցիչը դարձա՞վ դրական։",
+    "options": [
+    {
+    "a": "Այո",
+    "b": "Ոչ",
+
+    }
+    ],
+    "answer": "Ոչ",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 13,
+    "question": "Ո՞ր թվերն են իրար և գումարելիս, և բազմապատկելիս ստացվում նույն պատասխանը։",
+    "options": [
+    {
+    "a": "2 և 2",
+    "b": "3 և 6",
+    "c": "5 և 5",
+    "d": "4 և 2"
+    }
+    ],
+    "answer": "2 և 2",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 14,
+    "question": "Որ կենդանիները կարող են կանգնած քնել, սակայն նրանք կարող են երազներ տեսնել միայն պառկած վիճակում:",
+    "options": [
+    {
+    "a": "կովը",
+    "b": "փիղը",
+    "c": "ավանակը",
+    "d": "ձին"
+    }
+    ],
+    "answer": "կովը",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 15,
+    "question": " Ունի մի մուտք և 3 ելք: Դու մուտքով մտնում ես քեզ թվում է դու ներսում ես, բայց դու իրականում դրսում էլ ես լինում: Ինչն է դա:",
+    "options": [
+    {
+    "a": "վերնաշապիկ",
+    "b": "տաբատ"
+    }
+    ],
+    "answer": "վերնաշապիկ",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 16,
+    "question": "Սովորաբար հիվանդանոցները կառուցվում են քաղաքի ծայրամասերում, որպեսզի աղմուկից և տրանսպորտի արտանետումներից հեռու լինեն, սակայն Երևանի 1-ին կլինիկական հիվանդանոցը գտնվում է քաղաքի կենտրոնում: Ինչու՞:",
+    "options": [
+    {
+    "a": "Այն ժամանակ, երբ կառուցվել է հիվանդանոցը, այդ վայրը քաղաքի ծայրամասն էր:",
+    "b": "Այն ժամանակ, երբ կառուցվել է հիվանդանոցը, այդ վայրը քաղաքի կենտրոն էր:",
+    "c": "Այլ պատասխան"
+    }
+    ],
+    "answer": "Այն ժամանակ, երբ կառուցվել է հիվանդանոցը, այդ վայրը քաղաքի ծայրամասն էր:",
+    "score": 0,
+    "status": ""
+    },
+
+    {
+    "id": 17,
+    "question": "Քանի՞ աստղ կա Նոր Զելանդիայի դրոշի վրա․",
+    "options": [
+    {
+    "a": "երեք",
+    "b": "երկու",
+    "c": "չորս"
+    }
+    ],
+    "answer": "չորս",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 18,
+    "question": "Հյուրանոցն ունի 7 հարկ։ Առաջին հարկում ապրում է 4 մարդ, իսկ յուրաքանչյուր մյուս հարկում՝ 2-ով ավել, քան նախորդ հարկում։ Ո՞ր հարկում են ամենահաճախը վերելակ կանչում։",
+    "options": [
+    {
+    "a": "7-րդ հարկում",
+    "b": "Առաջինում։",
+    "c": "Նախորդ",
+   
+    }
+    ],
+    "answer": "7-րդ հարկում",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 19,
+    "question": "Ինչպե՞ս է կոչվում Հին Կտակարանի առաջին գիրքը․",
+    "options": [
+    {
+    "a": "Գիրք Ծնդոց",
+    "b": "Գիրք Ելից"
+    }
+    ],
+    "answer": "Գիրք Ծնդոց",
+    "score": 0,
+    "status": ""
+    }
+    ]
+
+
+
+    
+}
+
+    
+    
+    var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+    this.currentque = cque;
+    if (this.currentque < totalque) {
+    $("#tque").html(totalque);
+    $("#previous").attr("disabled", false);
+    $("#next").attr("disabled", false);
+    $("#qid").html(quiz.JS[this.currentque].id + '.');
+    $("#question").html(quiz.JS[this.currentque].question);
+    $("#question-options").html("");
+    for (var key in quiz.JS[this.currentque].options[0]) {
+    if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+    $("#question-options").append(
+    "<div class='form-check option-block'>" +
+    "<label class='form-check-label'>" +
+    "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+    quiz.JS[this.currentque].options[0][key] +
+    "</span></label>"
+    );
+    }
+    }
+    }
+    if (this.currentque <= 0) {
+    $("#previous").attr("disabled", true);
+    }
+    if (this.currentque >= totalque) {
+    $('#next').attr('disabled', true);
+    for (var i = 0; i < totalque; i++) {
+    this.score = this.score + quiz.JS[i].score;
+    }
+    return this.showResult(this.score);
+    }
+    }
+    this.showResult = function (scr) {
+    $("#result").addClass('result');
+    $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+    for (var j = 0; j < totalque; j++) {
+    var res;
+    if (quiz.JS[j].score == 0) {
+    res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+    } else {
+    res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+    }
+    $("#result").append(
+    '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+    '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+    '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+    '</div>'
+    );
+    }
+    }
+    this.checkAnswer = function (option) {
+    var answer = quiz.JS[this.currentque].answer;
+    option = option.replace(/</g, "&lt;") //for <
+    option = option.replace(/>/g, "&gt;") //for >
+    option = option.replace(/"/g, "&quot;")
+    if (option == quiz.JS[this.currentque].answer) {
+    if (quiz.JS[this.currentque].score == "") {
+    quiz.JS[this.currentque].score = 1;
+    quiz.JS[this.currentque].status = "correct";
+    }
+    } else {
+    quiz.JS[this.currentque].status = "wrong";
+    }
+    }
+    this.changeQuestion = function (cque) {
+    this.currentque = this.currentque + cque;
+    this.displayQuiz(this.currentque);
+    }
+    }
+    var jsq = new quizApp();
+    var selectedopt;
+    $(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+    //var radio = $(this).find('input:radio');
+    $(this).prop("checked", true);
+    selectedopt = $(this).val();
+    });
+    });
+    $('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+    });
+    $('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+    });
+
+   
